fc48686a1840b51f55743c2d1b3b5b55440c745f
erkan dormen <ekstremyazilim@gmail.com>
2021-06-11 17:49:27 +0300

    first commit

---
+++ counter/app.js
@@ -0,0 +1,30 @@
+//initial value
+let count = 0;
+//select value and buttons
+var value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const class_names = e.currentTarget.classList;
+
+    //change value
+    if (class_names.contains("decrease")) {
+      count--;
+    } else if (class_names.contains("reset")) {
+      count = 0;
+    } else {
+      count++;
+    }
+
+    //change color
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count === 0) {
+      value.style.color = "black";
+    } else {
+      value.style.color = "red";
+    }
+    value.textContent = count;
+  });
+});
