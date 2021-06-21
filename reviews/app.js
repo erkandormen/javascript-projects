e442b3bd1d3040982e44ffe219f640495ff9a22a
erkan dormen <ekstremyazilim@gmail.com>
2021-06-21 19:22:18 +0300

    Review Commit
    App JS completed

--- reviews/app.js
+++ reviews/app.js
@@ -38,6 +38,14 @@ const reviews = [
   },
 ];
 // select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
 
 
 // set starting item
@@ -53,15 +61,41 @@ window.addEventListener("DOMContentLoaded", function () {
 });
 
 // show person based on item
-function showPerson(person) {
-  const item = reviews[person];
+function showPerson(index) {
+  const item = reviews[index];
+
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
 }
+
 // show next person function
+nextBtn.addEventListener("click",
+function () {
+  currentItem++;
+  console.log("currentItem -> "+currentItem);
+  if (currentItem>reviews.length-1) {
+    currentItem = 0;
+  }
+showPerson(currentItem);
+});
 
 // show prev person function
-
-// show random person(Suprise Me button)
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  
+  if (currentItem < 0) {
+    currentItem = reviews.length-1;
+  }
+  console.log("currentItem -> "+currentItem);
+  showPerson(currentItem);
+});
+// show random review button
+randomBtn.addEventListener("click",function () {
+  let tmp = Math.random()*reviews.length;
+  console.log(tmp);
+  console.log(Math.floor(tmp));
+  currentItem = Math.floor(tmp);
+  showPerson(currentItem);
+});
